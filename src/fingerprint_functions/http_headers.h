832edbc8ac72b64ea6f1159c88a925b5961657ec
Thomas Sterrenburg <thomas@bit-students.com>
2017-01-24 23:26:12 +0100

    Adding licencing heading

--- src/fingerprint_functions/http_headers.h
+++ src/fingerprint_functions/http_headers.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2017 Thomas Sterrenburg
+ *
+ * Licensed under the MIT License (the License); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://opensource.org/licenses/MIT
+ */
+         
 #ifndef HTTP_HEADERS_H   /* Include guard */
 #define HTTP_HEADERS_H
 
--- src/fingerprint_functions/presence_in_reply.h
+++ src/fingerprint_functions/presence_in_reply.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2017 Thomas Sterrenburg
+ *
+ * Licensed under the MIT License (the License); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://opensource.org/licenses/MIT
+ */
+         
 #ifndef FINGERPRINTING_POC_PRESENCE_IN_FILE_H
 #define FINGERPRINTING_POC_PRESENCE_IN_FILE_H
 
