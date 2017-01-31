61950f749eb58a666f72befb906ab5e75668a909
Thomas Sterrenburg <thomas@bit-students.com>
2017-01-31 18:22:31 +0100

    Adding Lighttpd to docker servers

--- docker/servers.sh
+++ docker/servers.sh
@@ -14,13 +14,16 @@ APACHE_2_2_32_PORT=8082
 APACHE_2_4_25="apache-2.4.25"
 APACHE_2_4_25_PORT=8083
 
-SERVERS=`echo ${NGINX_1_11_9} ${NGINX_1_10_2} ${APACHE_2_2_32} ${APACHE_2_4_25}`
-PORTS=`echo ${NGINX_1_11_9_PORT} ${NGINX_1_10_2_PORT} ${APACHE_2_2_32_PORT} ${APACHE_2_4_25_PORT}`
-
-declare -a arr=(${SERVERS})
-declare -a arr2=(${PORTS})
+LIGHTTPD_1_4_39="lighttpd-1.4.39"
+LIGHTTPD_1_4_39_PORT=8084
 
 LOG=`echo $(dirname "$0")/docker.log`
+
+SERVERS=`echo ${NGINX_1_11_9} ${NGINX_1_10_2} ${APACHE_2_2_32} ${APACHE_2_4_25} ${LIGHTTPD_1_4_39}`
+PORTS=`echo ${NGINX_1_11_9_PORT} ${NGINX_1_10_2_PORT} ${APACHE_2_2_32_PORT} ${APACHE_2_4_25_PORT} ${LIGHTTPD_1_4_39_PORT}`
+
+declare -a servers_split=(${SERVERS})
+declare -a ports_split=(${PORTS})
 
 function start_servers {
     docker run -d -p ${NGINX_1_11_9_PORT}:${PORT_HOST} --name ${NGINX_1_11_9} nginx:1.11.9
@@ -28,6 +31,8 @@ function start_servers {
 
     docker run -d -p ${APACHE_2_2_32_PORT}:${PORT_HOST} --name ${APACHE_2_2_32} httpd:2.2.32
     docker run -d -p ${APACHE_2_4_25_PORT}:${PORT_HOST} --name ${APACHE_2_4_25} httpd:2.4.25
+
+    docker run -d -p ${LIGHTTPD_1_4_39_PORT}:${PORT_HOST} --name ${LIGHTTPD_1_4_39} sebp/lighttpd
 }
 
 function stop_servers {
@@ -39,26 +44,57 @@ function remove_servers {
 }
 
 function print_mapping {
-    printf "%s\t%s\t\t\t%s\n" "index" "container" "port"
+    printf "%s\t\t%s\t\t%s\n" "index" "container" "port"
     echo "------------------------------------"
 
-    for i in "${!arr[@]}"
+    for i in "${!servers_split[@]}"
     do
-       printf "%s\t\t%s\t\t%d->%d\n" "$i" "${arr[$i]}" "$PORT_HOST" "${arr2[$i]}"
+       printf "%s\t\t%s\t\t%d->%d\n" "$i" "${servers_split[$i]}" "$PORT_HOST" "${ports_split[$i]}"
        # or do whatever with individual element of the array
     done
 }
 
-if [ $# -ne 1 ];
-    then >&2 printf "$0 \nparameters:\n- run"
+function print_help {
+    >&2 printf "parameters:\n- start\n- stop\n- remove\n- list\n- info\n"
+}
+
+function print_info {
+    SERVERS_OR=''
+
+    for i in "${!servers_split[@]}"
+    do
+       SERVERS_OR="$SERVERS_OR${servers_split[$i]}|"
+    done
+
+    SERVERS_REGEX="CONTAINER|${SERVERS_OR::-1}"
+
+    OUTPUT="$(docker ps -a | grep -E $SERVERS_REGEX)"
+    echo "${OUTPUT}"
+}
+if [ $# -ne 1 ]; then
+    print_help
     exit 1
 fi
 
-if [ $1="run" ]; then
+if [ $1 == "start" ]; then
     printf "%s\n" "starting docker containers..."
 
-    stop_servers    > $LOG
-    remove_servers  > $LOG
-    start_servers   > $LOG
+    stop_servers    > $LOG 2>&1
+    remove_servers  > $LOG 2>&1
+    start_servers   > $LOG 2>&1
+    print_info
+elif [ $1 == "stop" ]; then
+    printf "%s\n" "stopping docker containers..."
+    stop_servers    > $LOG 2>&1
+elif [ $1 == "remove" ]; then
+    printf "%s\n" "removing docker containers..."
+    stop_servers    > $LOG 2>&1
+    remove_servers  > $LOG 2>&1
+elif [ $1 == "list" ]; then
     print_mapping
+elif [ $1 == "info" ]; then
+    print_info
+else
+    print_help
+    exit 1
 fi
